257979639f281fdefd9a0e9ec1ca0da0d3323b68
Archita Isha <156795223+archita-isha@users.noreply.github.com>
2025-08-23 13:18:52 +0000

    variables and datatype

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId= 12345
+let accountemail = "archita@google.com"
+var accountPassword = "archi"
+accountCity="Jamshedpur"
+let accountState;
+
+//accountId = 2 not allowed//
+
+accountemail = "isha@google.com"
+accountPassword = "ishi"
+accountCity = "Banglore"
+
+console.table([accountId , accountemail , accountPassword , accountCity , accountState])
+
+/*
+  Prefer ot to use var
+  because of block scope and functional scope
+*/
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,19 @@
+"use strict";// treat all js code as newer version
+
+//alert(3+3) we are using node js not browser
+
+
+/* Remember two documentation tc39.es  and mdn for js documentation
+*/
+
+//number 
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined => 
+//symbol => unique
+//object
+
+console.log(typeof undefined)//undefined
+console.log(typeof null) //object
