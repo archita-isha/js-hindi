6dfdabab475838d1f2c12820603f94c5a4dd7451
Archita Isha <156795223+archita-isha@users.noreply.github.com>
2025-08-25 13:43:59 +0000

    conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,38 @@
+/*let score = 33
+console.log(typeof score)
+console.log(typeof (score))//as method
+*/
+
+
+
+/*
+
+let score2="33abc"
+console.log(typeof score2)
+
+let valueInNum = Number(score2)
+console.log(typeof valueInNum)
+console.log(valueInNum)
+
+*/
+
+
+// "33"=> was converted to number
+//"33abc"=> was again converted to number , but value stored was NaN (not a number)
+// undefined => NaN
+//"hitesh" =>NaN
+//true (boolean) => 1 : false =>0
+
+
+let var3 = "hitest"
+
+let var4 = Boolean(var3)
+console.log(var4)
+
+//1=> true
+//"hitest"=>true
+//""=>false
+
+
+
+
