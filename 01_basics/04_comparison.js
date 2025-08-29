72a874faab59c02b483bf3516dc7f25daccfd60d
Archita Isha <156795223+archita-isha@users.noreply.github.com>
2025-08-29 16:07:57 +0000

    comparison operators study

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,28 @@
+// console.log(2>1)
+//console.log(2!=1)
+//console.log(2>=1)
+//console.log(2==1)
+
+
+/*
+console.log(null==0) =>false
+console.log(null>0) =>false
+console.log(null>=0) =>true
+*/
+
+//the reason that equality and comparison work differently because comparison converts null to 0 , and equality doesn't
+
+/* 
+console.log(undefined == 0)
+or with any other comparison gives ans as false
+
+*/
+
+//=== (strict cheack)
+
+/*
+console.log("2"==2)
+console.log("2"===2)
+*/
+// === will check the datatype too , in ("2"==2)  "2" will be converted to 2
+
