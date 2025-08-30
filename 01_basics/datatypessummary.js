69d38b4dad7cd6cd7f588995a4a5bf4a4deb7f33
Archita Isha <156795223+archita-isha@users.noreply.github.com>
2025-08-30 07:27:34 +0000

    about the pass by reference of non-primitive data type and the primitive data type

--- 01_basics/datatypessummary.js
+++ 01_basics/datatypessummary.js
@@ -16,7 +16,9 @@ let userEmail ; // ways to declare or use undefined
 
 const id = Symbol("123")
 const anotherId = Symbol("123")
+/*
 console.log(id === anotherId)
+*/
 
 const num = 2356789987654456768765n
 
@@ -28,12 +30,46 @@ const heros =['batman', 'superman', 'padman'];
 let myObj ={
     name : "archi",
     age: 22
-}
+}   //key value pair
 
 const myFun = function(){
     console.loh("hello world");
 }
 
+/*
 console.log(typeof num)
 console.log(typeof scoreValue )
 console.log(typeof myFun)  // datatype is object function
+*/
+
+
+//read datatypes on ecma 
+
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++=
+//two types of memory
+
+//primitive ->stack
+//non primitive ->heap
+
+let myName = "Archita"
+let anotherName = myName
+anotherName = "Isha"
+
+
+console.log(myName)
+console.log(anotherName)
+
+let userOne = {
+    email : "architaish@gmail.com",
+    upi : "archi@hdfc"
+}
+
+let userTwo = userOne
+userTwo.email = "isha@gmail.com"
+
+console.log(userOne.email)
+console.log(userTwo.email)
+
+
+//both objects email have changed so we can say that usertow got the reference of original object
