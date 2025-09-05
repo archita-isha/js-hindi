b1c6313bced4f8a8afc08c3ad41f406a33b7254c
Archita Isha <156795223+archita-isha@users.noreply.github.com>
2025-09-05 17:56:14 +0000

    arrays

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,44 @@
+//array
+const myArr = [0, 1, 2, 3, 4, "Isha"]
+//arrays resizable
+console.log(myArr[5])
+
+
+//shallow copies will give the copy the original reference
+//deep copies , properties do not share same reference 
+
+
+//can be declared
+const myArr2 = new Array(1, 2, 3, 4 )
+
+
+//Methods
+
+/*
+myArr.push(6) //adds ele at the end
+console.log(myArr)
+
+myArr.pop()
+console.log(myArr)
+*/
+/*
+Arr.unshift(0)    //appends ele at the starting , but will shift all the ele by one place , hence more timecomplexity if larger arr
+myArr.shift()
+console.log(myArr)
+*/
+
+/*
+console.log(myArr.includes(9))
+console.log(myArr.includes(0))
+
+console.log(myArr.indexOf(0))
+*/
+
+/*
+const newArr = myArr.join()
+console.log(myArr)
+console.log(newArr)
+console.log(typeof newArr)
+*/
+
+console.log("A" ,m)
